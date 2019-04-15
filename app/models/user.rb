7c7381407dd52608841325de77c73b9684bda881
Suraj Shrestha <ks.zenith@gmail.com>
2019-04-16 02:05:06 +0545

    relationship managed

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,16 +6,37 @@ class User < ApplicationRecord
 	has_many :relationships
 	has_many :relationship_types, class_name: 'Relationship', foreign_key: 'related_user_id'
 
+	GRAND_FATHER = RelationshipType.find_or_create_by(name: 'grandfather', reverse_name: 'grandson')
+	FATHER = RelationshipType.find_or_create_by(name: 'father', reverse_name: 'son')
+
 	def self.my_import(file)
 		users = []
 		CSV.foreach(file.path, headers: true) do |row|
-			binding.pry
-			users << User.new(row.to_h)
+			User.create_three_generations(row.to_h)
 		end
-	User.import users, recursive: true
 	end
 
-	def self.create_three_generations(row) 
+	# TODO: MOVE TO BACKGROUND JOB
 
+	def self.create_three_generations(row_hash) 
+		grand_father = User.find_or_create_by(name: row_hash["Grand Father"])
+		father = User.find_or_create_by(name: row_hash["Father"])
+		son = User.find_or_create_by(name: row_hash["Son"])
+
+		grand_father.relate(father, FATHER)
+		father.relate(son, FATHER)
+		grand_father.relate(son, GRAND_FATHER)
+
+		# Assumption: Grand Father lives with youngest grandson
+		grand_father.address = father.address = son.address = row_hash["Address"]
+
+		# TODO: REFACTOR
+		grand_father.save
+		father.save
+		son.save
+	end
+
+	def relate(related_user, relationship_type)
+	  self.relationships.find_or_create_by!(relationship_type_id: relationship_type.id, related_user_id: related_user.id)
 	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+grandfather =
+  RelationshipType.find_or_create_by_name! 'grandfather', reverse_name: 'grandson'
+father =
+  RelationshipType.find_or_create_by_name! 'father', reverse_name: 'son'
+
+sanulal =
+  User.find_or_create_by_name! 'Sanu Lal Shrestha'
+rajaram =
+  User.find_or_create_by_name! 'Raja Ram Shrestha'
+suraj =
+  User.find_or_create_by_name! 'Suraj Raj Shrestha'
+
+sanulal.relate(rajaram, father)
+rajaram.relate(suraj, father)
+sanulal.relate(suraj, grandfather)
